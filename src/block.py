470a457b25fb6f1946b062d6a7519e9390b99fdb
Andrew Nieto <andrewjnieto@gmail.com>
2023-07-10 12:55:22 -0500

    Experimenting with Logging

--- src/block.py
+++ src/block.py
@@ -3,48 +3,32 @@ from adguardhome import AdGuardHome
 import asyncio
 import json
 import datetime
+import logging
+
+def config_logs(log_config):
+    FORMAT='%(levelname)s|%(funcName)s: %(message)s [%(created)f]'
+    logging.basicConfig(filename='example.log',
+                        encoding='utf-8')
+    print('completed')
 
 def load_file(file_path):
     with open(file_path) as config_file:
         config = json.load(config_file)
         return config
 
-async def unlock_services(adguard):
+config_logs(load_file('../resources/config/logs.json'))
+logger = logging.getLogger();
+async def set_blocked_services(services, adguard):
     response = await adguard.request(uri='blocked_services/set',
                           method='POST',
-                          json_data=[])
-    if response['message'] == '':
-        print("Services successfully unlocked")
-    else:
-        print("Issue encountered unlocking services:", response['message'])
+                          json_data=services)
+    return response
 
-async def unlock_websites(adguard):
+async def set_blocked_websites(websites, adguard):
     response = await adguard.request(uri='filtering/set_rules',
                                      method='POST',
-                                     json_data={})
-    if response['message'] == '':
-        print("Websites successfully unlocked")
-    else:
-        print("Issue encountered unlocking websites:", response['message'])
-
-async def lock_services(blocked_services, adguard):
-    response = await adguard.request(uri='blocked_services/set',
-                                     method='POST',
-                                     json_data=blocked_services)
-    if response['message'] == '':
-        print("Services successfully locked")
-    else:
-        print("Issue encountered locking services:", response['message'])
-
- 
-async def lock_websites(blocked_websites, adguard):
-    response = await adguard.request(uri='filtering/set_rules',
-                                     method='POST',
-                                     json_data=blocked_websites)
-    if response['message'] == '':
-        print("Websites successfully locked")
-    else:
-        print("Issue encountered locking websites:", response['message'])
+                                     json_data=websites)
+    return response
 
 async def main():
     '''Show example how to get status of your AdGuard Home instance.'''
@@ -52,21 +36,24 @@ async def main():
     times = load_file('../resources/config/times.json')
     blocked_services = load_file('../resources/rules/blocked_services.json')
     blocked_websites = load_file('../resources/rules/blocked_websites.json')
+    log_config = load_file('../resources/config/logs.json')
+    config_logs(log_config)
+    logger.debug("debug")
     async with AdGuardHome(host=secrets['host'],
                            port=secrets['port'],
                            username=secrets['username'],
                            password=secrets['password']) as adguard:
         hour = datetime.datetime.now().hour
         if times['lock_hour'] == hour:
-            await lock_services(blocked_services, adguard)
-            await lock_websites(blocked_websites, adguard)
+            print('Blocking services and websites')
+            await set_blocked_services(blocked_services, adguard)
+            await set_blocked_websites(blocked_websites, adguard)
         elif times['unlock_hour'] == hour:
-            await unlock_services(adguard)
-            await unlock_websites(adguard)
+            print('Unblocking services and websites')
+            await set_blocked_services([], adguard)
+            await set_blocked_websites({}, adguard)
         else:
-            print('time to get a new watch')
-        print('It is now:', hour)
+            print('Time to get a new watch')
 
 if __name__ == "__main__":
-    loop = asyncio.new_event_loop()
-    loop.run_until_complete(main())
+    asyncio.run(main())
